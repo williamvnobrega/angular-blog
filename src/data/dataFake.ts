ad8a16ade1239c3afabee2af33ca424d8bb2fa09
William <williamvdnobrega@gmail.com>
2023-05-29 23:32:41 -0300

    style: addednew images and texts

--- src/data/dataFake.ts
+++ src/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
     {
         "id":"1",
         "title":"Enter the Chicken",
-        "description":"Produced By Serj Tankian, Enter The Chicken, the latest and greatest musical attraction at Bucketheadland is set to take riders on board from the second you push play.",
-        "photoCover":"https://cdn.shopify.com/s/files/1/1004/8262/products/enterthechicken_5000x.jpg?v=1445461172"
+        "description":"The ever mysterious and amazing talented guitarist and theme park manager, Buckethead has opened the doors to his famed Bucketheadland once again, and this time he has asked his friends to help him provide thrills and chills to his guests. Produced by Serj Tankian of System Of The Down.",
+        "photoCover":"https://preview.redd.it/qhthd40zpkv21.jpg?auto=webp&s=fb22b244855b04aef11330befcc7051e399cc56b"
     },
     {
         "id":"2",
-        "title":"saiu disco novo",
-        "description":"ola",
-        "photoCover":""
+        "title":"Pike 516",
+        "description":"Signed Drawing Edition will include a drawing and a signature on a blank CD sleeve as well as numbered CDR. Sale ends May 24.",
+        "photoCover":"https://f4.bcbits.com/img/a2014208792_10.jpg"
+    },
+    {
+        "id":"3",
+        "title":"Pike 515",
+        "description":"Signed Drawing Edition will include a drawing and a signature on a blank CD sleeve as well as numbered CDR. Sale ends May 24..",
+        "photoCover":"https://f4.bcbits.com/img/a2276025456_10.jpg"
+    },
+    {
+        "id":"4",
+        "title":"Pike 514",
+        "description":"Signed Drawing Edition will include a drawing and a signature on a blank CD sleeve as well as numbered CDR. Sale ends May 24..",
+        "photoCover":"https://f4.bcbits.com/img/a0726454226_10.jpg"
     }
 ]
